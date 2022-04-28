640995badff4a43e379ac6198f5d708d627202d0
zohaib166 <zohaib166@gmail.com>
2022-04-28 19:12:41 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
 import Todo from './components/Todo';
+
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
-      <Todo />
-      <Todo />
+      <Todo text='Learn React'/>
+      <Todo text='Master React'/>
+      <Todo text='Explore React'/>
     </div>
   );
 }
